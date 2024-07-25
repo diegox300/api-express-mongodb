188bbb6211dbf2005932fe5d5867e69665c3d808
Diego Gervasio <diegox300@gmail.com>
2024-07-25 11:53:39 -0300

    Conectando ao banco mongoose

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+console.error("erro de conexao", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexao com o banco feita com sucesso.");
+})
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo: "O hobbit"
-    }
-]
-
-function buscarLivro(id) {
-    return livros.findIndex(livros => {
-        return livros.id === Number(id);
-    });
-}
 
 app.get("/", (req,res) => {
 res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req,res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req,res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req,res) => {
@@ -51,3 +47,4 @@ app.delete("/livros/:id", (req,res) => {
 })
 
 export default app;
+
