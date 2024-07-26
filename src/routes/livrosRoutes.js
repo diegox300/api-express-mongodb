4c68c698b48306a9ba4439a32d477c8c50996134
Diego Gervasio <diegox300@gmail.com>
2024-07-26 17:06:13 -0300

    Fix: Mocando senha do banco.

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
 routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
-routes.get("/livros/:id", LivroController.listarLivroPorId)
+routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
 routes.delete("/livros/:id", LivroController.excluirLivro );
